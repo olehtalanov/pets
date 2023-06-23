d83179a4be8162930d4e0ad49c6b68a9cb2552b5
Oleh <talanov.o@gmail.com>
2023-06-23 16:50:21 +0300

    adjust appeal email template

--- resources/views/mail/admin/appeal.blade.php
+++ resources/views/mail/admin/appeal.blade.php
@@ -1,5 +1,5 @@
 <x-mail::message>
-### {{ trans('notifications.admin.appeal.line_a', ['user' => $appeal->user->name]) }}
+## {{ trans('notifications.admin.appeal.line_a', ['user' => $appeal->user->name]) }}
 
 Email: <a href="mailto:{{ $appeal->user->email }}">{{ $appeal->user->email }}</a>
 
@@ -7,7 +7,7 @@ Email: <a href="mailto:{{ $appeal->user->email }}">{{ $appeal->user->email }}</a
 {{ $appeal->message }}
 
 @if($appeal->rating)
-<div style="display: flex; gap: 0 5px;">
+<div style="display: flex; gap: 0 2px;">
 @for($i = 1; $i <= 5; $i++)
 @if($i <= $appeal->rating)
 <img src="{{ asset('images/star-filled.svg') }}" alt="" style="display: block; width: 20px; height: 20px;">
